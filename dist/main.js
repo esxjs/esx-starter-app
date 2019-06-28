df171bfd291b7d4cbf5e1c2974d1a98007f27ad1
David Mark Clements <huperekchuno@googlemail.com>
2019-06-28 13:18:39 +0200

    logging

--- dist/main.js
+++ dist/main.js
@@ -32711,7 +32711,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63618" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52838" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict'
 const path = require('path')
 const AutoLoad = require('fastify-autoload')
+const { extreme, final } = require('pino')
 
 function server (fastify, opts, next) {
   fastify.register(AutoLoad, {
@@ -11,11 +12,51 @@ function server (fastify, opts, next) {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
   })
+
+  configureLogger(fastify)
+
   next()
 }
 
 server.options = {
-  logger: { level: 'silent' }
+  logger: { stream: extreme(1) }
 }
 
 module.exports = server
+
+function configureLogger(fastify) {
+  // asynchronously flush every 10 seconds to keep 
+  // logging the buffer empty in periods of low activity
+  setInterval(() => {
+    fastify.log.flush()
+  }, 10000).unref()
+
+  // use pino.final to create a special logger that
+  // guarantees final tick writes
+  const handler = final(fastify.log, (err, finalLogger, evt) => {
+    finalLogger.info(`${evt} caught`)
+    if (err) finalLogger.error(err, 'error caused exit')
+    process.exit(err ? 1 : 0)
+  })
+  // catch all the ways node might exit
+  process.on('beforeExit', () => handler(null, 'beforeExit'))
+  process.on('exit', () => handler(null, 'exit'))
+  process.on('uncaughtException', (err) => handler(err, 'uncaughtException'))
+  process.on('SIGINT', () => handler(null, 'SIGINT'))
+  process.on('SIGQUIT', () => handler(null, 'SIGQUIT'))
+  process.on('SIGTERM', () => handler(null, 'SIGTERM'))
+
+
+  // flush out the first logger messages after server start
+  fastify.ready(() => {
+    const { info } = fastify.log
+    fastify.log.info = (...args) => {
+      const result = info.apply(fastify.log, args)
+      fastify.log.flush()
+      return result
+    }
+    setTimeout(() => {
+      fastify.log.info = info
+    }, 100)
+  })
+}
